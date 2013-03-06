bb8c5db970b0004b98355d6a641bcc4695c5d246
Robin Böhm <robinboehm@googlemail.com>
2013-03-06 12:19:10 +0100

    Added talk mock array to the MainCtrl $scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,21 @@
 
 angular.module('ruhrjsApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Testacular'
+
+    $scope.talks = [
+        {
+        time : "06.03.2011 12:00",
+        title : "Creating the RuhrJS Website",
+        speakers: [
+            {
+                firstname: "Robin"
+            },
+            {
+                firstname: "Philipp"
+            }
+         ],
+            description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum"
+        }
     ];
+        
   });
