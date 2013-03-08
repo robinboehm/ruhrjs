6185f6c174c35fa54ea44a5e8ff626713a3f72e9
Robin Böhm <robinboehm@googlemail.com>
2013-03-08 11:05:50 +0100

    Extend talkProvider with empty slots api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,5 @@
 angular.module('ruhrjsApp')
   .controller('MainCtrl', function ($scope, talkprovider) {
     $scope.talks = talkprovider.getTalks();
+    $scope.freeSlots = talkprovider.getFreeSlots();
   });
--- app/scripts/services/talkprovider.js
+++ app/scripts/services/talkprovider.js
@@ -4,25 +4,23 @@ angular.module('ruhrjsApp')
   .factory('talkprovider', function () {
     // Service logic
 
+    var slotSize = 3;
+
+    var generateEmtpyText = function(){
+        return{
+            title: "Free Slot",
+            description : "You’re doing something interesting stuff with JavaScript more people should know about?",
+            twitter :   "https://twitter.com/RuhrJS/",
+            mailingList : "https://groups.google.com/group/ruhrjs"
+        };
+    };
+
     // Mock of the Talks
     // Possible extension via $http service
     var talks = [
         {
             time : "06.03.2011 12:30",
             title : "Using TalkProvider",
-            speakers: [
-                {
-                    firstname: "Robin"
-                },
-                {
-                    firstname: "Philipp"
-                }
-            ],
-            description: "Provide Impsum Provide Impsum  Provide Impsum  Provide Impsum  Provide Impsum  Provide Impsum"
-        },
-        {
-            time : "06.03.2011 14:30",
-            title : "Creating a basic Design for RuhrJS",
             speakers: [
                 {
                     firstname: "Robin"
@@ -52,6 +50,16 @@ angular.module('ruhrjsApp')
     return {
       getTalks: function () {
         return talks;
+      },
+      getFreeSlots: function(){
+          var freeSlots = Array();
+          var currentSlot = talks.length;
+
+          for(;currentSlot<slotSize;currentSlot++){
+              freeSlots.push(generateEmtpyText());
+          }
+          return freeSlots;
       }
+
     };
   });
